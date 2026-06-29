79ddadbcae1a8201a52b4f8f16b6c3c4f04654a9
Lenhan231 <nhanle221199@gmail.com>
2026-06-29 18:53:21 +0700

    response body

--- app/routers/matching.py
+++ app/routers/matching.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 
-from app.schemas import MatchRequest
+from app.schemas import MatchRequest, MatchResponse
 from app.services.matching_service import evaluate_cv_match
 
 router = APIRouter(tags=["matching"])
 
 
-@router.post("/evaluate")
+@router.post("/evaluate", response_model=MatchResponse)
 def evaluate(req: MatchRequest):
     try:
         response = evaluate_cv_match(req)
-        return JSONResponse(content=response)
+        return response
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
